ccfd23e1c7da51c35c16974ead9a06b775621067
Michael <mikefammc@gmail.com>
2018-09-25 22:57:56 -0400

    Finished Web Caesar : Update 1

--- web-caesar/main.py
+++ web-caesar/main.py
@@ -11,35 +11,30 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
-      <div class="wrapper">
-            <div class="inner-wrapper">
                 <h1>Caesar Cipher</h1>
                 <p>Type something in and see your message encrypted!</p>
-                <hr>
                 <form class="cipher-form" action ="/" method="POST">
                     <textarea name="text" placeholder="Enter Message to Encrypt:">{0}</textarea>
-                    <label for "rot">Enter Amount to Rotate:</label>
+                    <label for "rot">Enter Amount to rotate:</label>
                     <input type="text" id="rot" name="rot" value="0">
                     <input type="submit" value="Encrypt">
                 </form>
-            </div>
-        </div>
         <div class="footer">
             <p class="footer">Designed by Michael Montalvan</p>
         </div>
